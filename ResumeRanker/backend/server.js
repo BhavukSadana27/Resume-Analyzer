b91ffd791ad47808ad4de80acc35ebad804e5204
bhavuk <bhavuksadana@gmail.com>
2025-08-04 23:16:57 +0530

    fixed the bugs

--- ResumeRanker/backend/server.js
+++ ResumeRanker/backend/server.js
@@ -1,5 +1,7 @@
 import app from "./index.js";
 
-app.listen(process.env.PORT, () => {
+app.listen(process.env.PORT || 8000, () => {
   console.log(`Server is running at http://localhost:${process.env.PORT}`);
-});
+}).on('error', (err) => {
+  console.error('Failed to start server:', err.message);
+})
